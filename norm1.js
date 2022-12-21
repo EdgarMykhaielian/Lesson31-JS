cd9c66cdbb2fc50e01f40ec0e325fcfb037fba39
Eddy <edgarmykhaielian@gmail.com>
2022-12-21 15:42:59 +0200

    rewrite palindrome task with do/while

--- norm1.js
+++ norm1.js
@@ -1,17 +1,29 @@
 alert('Task 1: Palindrome')
-function ifPalindrome(userNumber = prompt('Type in a 5-digit number and i will tell if it is a palindrome.')){
-let regex = /^\d{5}$/
-let Palindrome = userNumber.split('').reverse().join('');
-if (userNumber.match(regex)) {
+isPalindrome(getValidNumberFromUser())
+alert('Next task')
+
+function isPalindrome(userNumber) {
+  let Palindrome = userNumber.split('').reverse().join('');
+
   if (userNumber === Palindrome) {
     alert(`Your number ${userNumber} is a Palindrome!`)
   }
   else {
     alert(`Your number ${userNumber} is NOT a Palindrome`)
   }
-} else {
-  alert('Please type in a 5-digit number!')
 }
+
+function getValidNumberFromUser() {
+  const regex = /^\d{5}$/
+  let str = 'Type in a 5-digit number and I will tell if it is a palindrome.'
+  let userNumber
+
+  do {
+    userNumber = prompt(str) || ''
+    str = 'Please type in a 5-digit number!'
+  } while (!userNumber.match(regex))
+
+  return userNumber
 }
-ifPalindrome()
-alert('Next task')
+
+
--- norm2.js
+++ norm2.js
@@ -1,20 +1,24 @@
-alert('Task 2: Let us find out your discount!')
-let sum = +prompt('What is the sum of your purchases?')
-let discount = 0
-if (sum < 200) {
-    alert('Your sum must be at least 200$ to get a discount.')
+alert('Task 2: Let us find out the sum of your invoice and your discount!')
+function invoiceSum(sum) {
+    let discount;
+    sum = +prompt('What is the sum of your purchases?')
+
+    if (sum < 200) {
+        alert('Your sum must be at least 200$ to get a discount.')
+    }
+    else if (sum < 300) {
+        discount = (sum * 0.03).toFixed(2)
+        alert(`Your discount is 3%. Total sum: ${sum - discount}$`)
+    }
+    else if (sum < 500) {
+        discount = (sum * 0.05).toFixed(2)
+        alert(`Your discount is 5%. Total sum: ${sum - discount}$`)
+    }
+    else if (sum >= 500) {
+        discount = (sum * 0.07).toFixed(2)
+        alert(`Your discount is 7%. Total sum: ${sum - discount}$`)
+    }
+    else { alert('Please enter a valid number') }
 }
-else if (sum < 300) {
-    discount = (sum * 0.03).toFixed(2)
-    alert(`Your discount is 3%. Total sum: ${sum - discount}$`)
-}
-else if (sum < 500) {
-    discount = (sum * 0.05).toFixed(2)
-    alert(`Your discount is 5%. Total sum: ${sum - discount}$`)
-}
-else if (sum >= 500) {
-    discount = (sum * 0.07).toFixed(2)
-    alert(`Your discount is 7%. Total sum: ${sum - discount}$`)
-}
-else { alert('Please enter a valid number') }
+invoiceSum()
 alert('Next')
